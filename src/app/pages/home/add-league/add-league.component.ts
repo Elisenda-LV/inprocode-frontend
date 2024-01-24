5eb788c789c3ed8194a06c5f9bd18272b4a4563d
Elisenda-LV <elisenda.lv@gmail.com>
2024-01-24 23:38:22 +0100

    home, service, interface

--- src/app/pages/home/add-league/add-league.component.ts
+++ src/app/pages/home/add-league/add-league.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { League } from '../../../interfaces/league.interface';
+import { LeagueService } from '../../../services/league.service';
+
+
 
 @Component({
   selector: 'app-add-league',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule,  ],
   templateUrl: './add-league.component.html',
   styleUrl: './add-league.component.scss'
 })
-export class AddLeagueComponent {
+
+
+export class AddLeagueComponent  {
+
+  activeModal  = inject(NgbActiveModal);
+
+  @Input() league: League[] = [];
+
+  leagueForm = new FormGroup({
+    name: new FormControl (''),
+    sport: new FormControl (''),
+    category: new FormControl (''),
+    location: new FormControl ('')
+
+  });
+
+  constructor(public leagueService: LeagueService){}
+
+
+  createLeague(){
+    if(this.leagueForm.valid){
+
+      const formValues = this.leagueForm.value;
+      const newLeague: any = {
+        name: formValues.name!,
+        sport: formValues.sport!,
+        category: formValues.category!,
+
+      }
+
+      this.leagueService.addLeague(newLeague).subscribe(
+        {
+          next: (createdLeague: any) => {
+            this.activeModal.close(createdLeague)
+          },
+          error: (err) => console.log(err)
+        }
+      )
+    }
+  }
 
 }
--- src/app/pages/home/edit-league/edit-league.component.ts
+++ src/app/pages/home/edit-league/edit-league.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject, Input } from '@angular/core';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { League } from '../../../interfaces/league.interface';
+import { LeagueService } from '../../../services/league.service';
+
+
 
 @Component({
   selector: 'app-edit-league',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './edit-league.component.html',
   styleUrl: './edit-league.component.scss'
 })
-export class EditLeagueComponent {
+
+
+export class EditLeagueComponent implements OnInit {
+
+  activeModal = inject(NgbActiveModal);
+
+  @Input() leagues: League | undefined;
+
+  leagueForm: FormGroup;
+
+  constructor( public leagueService: LeagueService ){
+    this.leagueForm = new FormGroup({
+      name: new FormControl (''),
+      sport: new FormControl (''),
+      category: new FormControl (''),
+      location: new FormControl (''),
+    })
+
+  }
+
+  ngOnInit(): void {
+    this.resetForm()
+  }
+
+  resetForm():void {
+    this.leagueForm.reset(this.leagues)
+  }
+
+  editLeague(){
+    if(this.leagueForm.valid){
+      const formValues = this.leagueForm.value;
+
+      console.log(this.leagues);
+
+      const editLeagues: League = {
+        id: this.leagues?.id!,
+        name: formValues.name,
+        sport: formValues.sport,
+        category: formValues.category,
+
+      }
+
+      this.leagueService.updateLeague(editLeagues).subscribe(
+        {
+          next: () => {
+            this.activeModal.close(editLeagues);
+          },
+          error: (err) => console.log(err)
+        }
+      );
+    }
+  }
+
 
 }
